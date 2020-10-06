4292b160c3aee060328893448929a6e0801a4736
Tanishq <sehratanishq@gmail.com>
2020-10-07 01:43:15 +0530

    Added Tanishq Sehra's commit

--- user.js
+++ user.js
@@ -27,7 +27,8 @@ let cards = [
     ["Anthony Morris", "./img/Anthony-Morris.png", "SE", "https://github.com/TonyDMorris"],
     ["V-50", "./img/username.png", "Student", "https://github.com/v-50"],
     ["Kunal Bhatia", "./img/kunalbhatia18.jpg", "Student ", "https://github.com/kunalbhatia18"],
-    ["Wisnu Febriadi", "./img/wfebriad.jpg", "Student ", "https://github.com/wfebriad"]
+    ["Wisnu Febriadi", "./img/wfebriad.jpg", "Student ", "https://github.com/wfebriad"],
+    ["Tanishq Sehra","./img/tanishq1502.jpg","B.E CSE Student","https://github.com/tanishq1502"]
 
     
 ]
