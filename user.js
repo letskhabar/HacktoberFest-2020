822c6507257d3df233e7bc05df1abc6eca70ba31
Andrean Novan <andreannovan22@gmail.com>
2020-10-18 00:01:09 +0700

    Edit User.js and add image

--- user.js
+++ user.js
@@ -42,6 +42,7 @@ let cards = [
     ["Mohd Fahad Khan", "./img/fahad.jpg", "Full Stack Software Developer", "https://github.com/codevfahad"],
     ["Carolline Souza", "./img/carolladss.jpg", "Front-End Developer Student", "https://github.com/carolladss"],
     ["Cormac Krum","./img/cormacKrum.jpg","You Know Who I Am","https://github.com/CormacKrum"],
+    ["Andrean Novan","./img/andreanz22.jpg","Web Dev","https://github.com/andreanz22"],
 
 
     
