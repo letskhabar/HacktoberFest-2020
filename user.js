8d22f3edbfa00cdab6d651a357cefa7c800eb1ea
kashish-goel <71588264+kashish-goel@users.noreply.github.com>
2020-10-30 23:03:03 +0530

    Update user.js

--- user.js
+++ user.js
@@ -55,6 +55,7 @@ let cards = [
     ["Kuroyza", "./img/kuroyza.jpg", "A full stack developer who enjoys watching anime", "https://github.com/kuroyza"],
     ["Felipe Rodrigues", "Image URL(./img/feliperodrigues.png)", "Student", "https://github.com/felipprodrigues"],
     ["Muhammad ALiyul Murtadlo", "./img/username.png", "Student", "https://github.com/mmdiyul"],
+    ["Kashish Goel", "./img/kashish-goel.jpg", "Student", "https://github.com/kashish-goel"],
   
   
   
